1657d6a0b0b19dc9067923474548a3bf09dc7f35
Jagdanand Jha <107176952+JagdanandJha@users.noreply.github.com>
2024-04-18 11:38:02 +0530

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Web3 } from 'web3';
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 const web3 = new Web3(window.ethereum);
 
-const pinataApiKey = '4aca2fc6337ab8d50897';
-const pinataSecretApiKey = '47336afd2c3829af8903acd3c8ec3ec2dfeae1af91a0e88c3d67759b6d6a971d';
+const pinataApiKey = process.env.pinataApiKey;
+const pinataSecretApiKey = process.env.pinataSecretApiKey;
 
 const Create = ({ account, nft, marketPlace }) => {
   const navigate = useNavigate();
